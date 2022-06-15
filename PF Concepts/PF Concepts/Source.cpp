7bcadd71a5c8a8f3f86c31c9344b3bf5d7d4eb02
Osama Sultan <osamasultan189@gmail.com>
2022-06-15 23:36:30 +0500

    print duplicates

--- PF Concepts/PF Concepts/Source.cpp
+++ PF Concepts/PF Concepts/Source.cpp
@@ -4,6 +4,41 @@ using namespace std;
 //	int c = 4;
 //	*pointer = c;
 //}
+void printDuplicates(int arr[], int n)
+{
+	int i;
+	// Flag variable used to
+	// represent whether repeating
+	// element is found or not.
+	int fl = 0;
+	for (i = 0; i < n; i++) {
+		// Check if current element is
+		// repeating or not. If it is
+		// repeating then value will
+		// be greater than or equal to n.
+		if (arr[arr[i] % n] >= n) {
+			// Check if it is first
+			// repetition or not. If it is
+			// first repetition then value
+			// at index arr[i] is less than
+			// 2*n. Print arr[i] if it is
+			// first repetition.
+			if (arr[arr[i] % n] < 2 * n) {
+				cout << arr[i] % n << " ";
+				fl = 1;
+			}
+		}
+		// Add n to index arr[i] to mark
+		// presence of arr[i] or to
+		// mark repetition of arr[i].
+		arr[arr[i] % n] += n;
+	}
+	// If flag variable is not set
+	// then no repeating element is
+	// found. So print -1.
+	if (!fl)
+		cout << "-1";
+}
 void removeDuplicates(int arr[],int size) {
 	int* newArray = new int[size];
 	int count = 1;
@@ -82,4 +117,10 @@ int main() {
 	removeDuplicates(Input, 8);*/
 
 
+
+	//******Print duplicates from array
+
+
+	/*int Input[8] = { 0,1,2,1,3,2,3,5 };
+	printDuplicates(Input, 8);*/
 }
